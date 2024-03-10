6452140e8a07e14957f34f620fc206e1dbd9af33
Marshallhch <marshallhch@gmail.com>
2024-03-10 19:36:06 +0900

    create user data in convex db

---
+++ app/(routes)/dashboard/page.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { api } from '@/convex/_generated/api';
+
+import {
+  LogoutLink,
+  useKindeBrowserClient,
+} from '@kinde-oss/kinde-auth-nextjs';
+import { useMutation, useQuery } from 'convex/react';
+import React, { useEffect } from 'react';
+
+function Dashboard() {
+  const { user }: any = useKindeBrowserClient();
+  const getUser = useQuery(api.user.getUser, { email: user?.email });
+
+  const createUser = useMutation(api.user.createUser);
+
+  useEffect(() => {
+    if (user) {
+      if (getUser === undefined) {
+        createUser({
+          name: user.given_name,
+          email: user.email,
+          image: user.picture,
+        }).then((res) => {
+          console.log(res);
+        });
+      }
+    } else {
+    }
+  }, [user]);
+  return (
+    <div>
+      Dashboard
+      <Button>
+        <LogoutLink>Logout</LogoutLink>
+      </Button>
+    </div>
+  );
+}
+
+export default Dashboard;
